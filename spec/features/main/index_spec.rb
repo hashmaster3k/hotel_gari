f7ebeaf485eddda8341b50359effc3c317cb578a
Hashim Gari <hashmaster3k@gmail.com>
2020-09-03 14:50:57 -0600

    Update testing for form visibility

--- spec/features/main/index_spec.rb
+++ spec/features/main/index_spec.rb
@@ -11,4 +11,22 @@ RSpec.describe 'MAIN INDEX PAGE' do
     visit '/'
     expect(page).to have_xpath("//img['#{'overview.jpg'}']")
   end
+
+  it 'has three other images' do
+    visit '/'
+    expect(page).to have_xpath("//img['#{'img_1.jpg'}']")
+    expect(page).to have_xpath("//img['#{'img_2.jpg'}']")
+    expect(page).to have_xpath("//img['#{'img_3.jpg'}']")
+  end
+
+  it 'has a quick booking search box' do
+    visit '/'
+
+    within '.search-box'do
+      expect(page).to have_content('Check-in')
+      expect(page).to have_content('Check-out')
+      expect(page).to have_content('Adults')
+      expect(page).to have_content('Children')
+    end
+  end
 end
