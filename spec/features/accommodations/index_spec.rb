762e92a01018eabcfae2fbd12df09a22f673ce84
Hashim Gari <hashmaster3k@gmail.com>
2020-09-17 11:29:08 -0600

    Fixes test due to col addition to db

--- spec/features/accommodations/index_spec.rb
+++ spec/features/accommodations/index_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe 'ACCOMMODATIONS INDEX PAGE'do
   end
 
   it 'displays hotel room statistics' do
-    Room.create!(beds: 1, price: 99.99)
-    Room.create!(beds: 1, price: 119.99, river_view: true)
-    Room.create!(beds: 2, price: 129.99)
-    Room.create!(beds: 2, price: 149.99, river_view: true)
+    Room.create!(image: '1.jpg', beds: 1, price: 99.99, description: 'wow')
+    Room.create!(image: '2.jpg', beds: 1, price: 119.99, description: 'wow', river_view: true)
+    Room.create!(image: '3.jpg', beds: 2, price: 129.99, description: 'wow')
+    Room.create!(image: '4.jpg', beds: 2, price: 149.99, description: 'wow', river_view: true)
 
     visit '/accommodations'
 
--- spec/features/bookings/index_spec.rb
+++ spec/features/bookings/index_spec.rb
@@ -3,7 +3,64 @@
 require 'rails_helper'
 
 RSpec.describe 'BOOKINGS INDEX PAGE' do
-  it 'has a valid link to bookings' do
-    visit '/bookings'
+  before :each do
+    @room_1 = Room.create!(image: 'carousel_3.jpg', beds: 1, price: 99.99, description: 'wow')
+    @room_2 = Room.create!(image: 'carousel_3.jpg', beds: 1, price: 119.99, description: 'wow', river_view: true)
+    @room_3 = Room.create!(image: 'carousel_6.jpg', beds: 2, price: 129.99, description: 'wow')
+    @room_4 = Room.create!(image: 'carousel_6.jpg', beds: 2, price: 149.99, description: 'wow', river_view: true)
+  end
+
+  describe 'a visitor' do
+    it 'has a valid link to bookings' do
+      visit '/bookings'
+    end
+
+    it 'has a search box for booking' do
+      visit '/bookings'
+
+      within '.search-box-booking'do
+        expect(page).to have_content('Check-in')
+        expect(page).to have_content('Check-out')
+        expect(page).to have_content('Adults')
+        expect(page).to have_content('Children')
+        expect(page).to have_button('FIND VILLA')
+      end
+    end
+
+    it 'lists all current available rooms' do
+      visit '/bookings'
+
+      within "#room-#{@room_1.id}" do
+        expect(page).to have_xpath("//img['#{@room_1.image}']")
+        expect(page).to have_content("#{@room_1.beds} Bed Villa")
+        expect(page).to have_content(@room_1.description)
+        expect(page).to have_content(@room_1.price)
+        expect(page).to have_button("BOOK")
+      end
+
+      within "#room-#{@room_2.id}" do
+        expect(page).to have_xpath("//img['#{@room_2.image}']")
+        expect(page).to have_content("#{@room_2.beds} Bed Villa | River View")
+        expect(page).to have_content(@room_1.description)
+        expect(page).to have_content(@room_2.price)
+        expect(page).to have_button("BOOK")
+      end
+
+      within "#room-#{@room_3.id}" do
+        expect(page).to have_xpath("//img['#{@room_3.image}']")
+        expect(page).to have_content("#{@room_3.beds} Bed Villa")
+        expect(page).to have_content(@room_1.description)
+        expect(page).to have_content(@room_3.price)
+        expect(page).to have_button("BOOK")
+      end
+
+      within "#room-#{@room_4.id}" do
+        expect(page).to have_xpath("//img['#{@room_4.image}']")
+        expect(page).to have_content("#{@room_3.beds} Bed Villa | River View")
+        expect(page).to have_content(@room_1.description)
+        expect(page).to have_content("$#{@room_4.price}")
+        expect(page).to have_button("BOOK")
+      end
+    end
   end
 end
