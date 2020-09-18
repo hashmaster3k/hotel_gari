28af15b4e643f82c2147ff21d3a57bfd3ce7aea5
Hashim Gari <hashmaster3k@gmail.com>
2020-09-18 08:29:08 -0600

    Test for manual creation of reservation

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -15,4 +15,25 @@ RSpec.describe Reservation, type: :model do
     it { should belong_to :room }
     it { should belong_to :user }
   end
+
+  it 'can create a reservation' do
+    room = Room.create!(image: '1.jpg', beds: 1, price: 99.99, description: 'Wow')
+
+    user = User.create!(username: 'user',
+                        password: '123',
+                        first_name: 'Joe',
+                        last_name: 'James',
+                        address: '1382 JJ Drive',
+                        city: 'Denver',
+                        state: 'CO',
+                        zip: 80210)
+
+    user.reservations.create!(room_id: room.id,
+                              check_in: Date.today,
+                              check_out: Date.today,
+                              adults: 1,
+                              children: 0)
+
+    expect(Reservation.count).to eq(1)
+  end
 end
